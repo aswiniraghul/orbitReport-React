3610fcf7675589977497affecece96c7a67c1295
Aswini Raghul <aswiniraghul@gmail.com>
2024-03-18 13:19:29 -0500

    completed Graded Assignment #6

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,35 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+
+import satData from "./satData";
+import './styling.css'
+
+
+// const Buttons = ({ filterByType, setSat, displaySats }) => {
+//   return (
+//     <div>
+//       <button>Placeholder Button</button>
+//       <button>All Orbits</button>
+//     </div>
+//   );
+// };
+
+const Buttons = (props) => {
+  const filterByType = props.filterByType;
+  const setSat = props.setSat;
+  const displaySats = props.displaySats;
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
+    <div className='flex-container'>
+      {displaySats.map((sat, id) => {
+        
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+      </div>
   );
 };
+
 
 export default Buttons;
